a698e6ad340628c56b7716fe6f6960b1738b2a74
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2023-05-17 20:29:43 -0400

    agregando eventos fail

--- assets/js/calculos.js
+++ assets/js/calculos.js
@@ -19,7 +19,7 @@ const infoIn = document.getElementById('ingreso-registrado')
 const infoE = document.getElementById('gasto-registrado')
 const infoTotal = document.getElementById('total')
 //seccion gatos detallados
-
+const detalle = document.getElementById('detalle')
 
 //iconos para tipos de gastos
 let otros = "<box-icon name='dollar'></box-icon>"
@@ -27,16 +27,16 @@ let mercaderia = "<box-icon name='store'></box-icon>"
 let medico = "<box-icon name='plus-medical'></box-icon>"
 let transporte = "<box-icon name='bus' ></box-icon>"
 let imprevisto = "<box-icon name='error' type='solid' ></box-icon>"
-let lujos = "<box-icon name='crown' ></box-icon>"
+let lujo = "<box-icon name='crown' ></box-icon>"
 let basura = "<box-icon name='trash' type='solid' ></box-icon>"
 //variables axiliares que ayudan con el registro de los mmontos de ingresos y egresos
 let seguimiento=[]
-function total(){
+let id = 1
 
-}
 class DatoFinanciero{
     constructor(valor, tipo){
         this[tipo]=  parseInt(valor)
+        this.valor = valor
     }
    insertar(){
     seguimiento.push(this)
@@ -61,8 +61,29 @@ class DatoFinanciero{
 }
 
 class Egreso extends DatoFinanciero{
-    constructor(valor,tipo,nombre, subTipo, id){
+    constructor(valor,tipo,nombre, icono, iconoBasura, id){
         super(valor, tipo)
+        
+        this.nombre= nombre
+        this.icono= icono
+        this.boton=iconoBasura
+        this.id= id
+    }
+    insertar(){
+        return super.insertar()
+    }
+    leerYEscribir(){
+        return super.leerYEscribir()
+    }
+    elemento(){
+        let componente= `<div class='componente_detalle'><span>${this.nombre}</span><span>${this.icono}</span> <span>${this.valor}</span> <button id="${this.id}">${this.boton}</button></div>`
+        detalle.innerHTML += componente
+        return componente
+    }
+    addevento(id){
+        document.getElementById(`${id}`).addEventListener('click', ()=>{
+            console.log('hola mundo');
+        })
     }
 }
 //validacion de datos ingresados para descartar los no numericos y dar avisos
@@ -71,14 +92,44 @@ ingresos.addEventListener('change', ()=>{
 })
 
 btnRegistroIn.addEventListener('click', ()=>{
-    
-        let auxiliar = new DatoFinanciero(ingresos.value, ingresos.id )
-        auxiliar.insertar()
-        auxiliar.leerYEscribir()
-        ingresos.value = ""
-    
+    let auxiliar = new DatoFinanciero(ingresos.value, ingresos.id )
+    auxiliar.insertar()
+    auxiliar.leerYEscribir()
+    ingresos.value = ""
 })
 btnContinuar.addEventListener('click',()=>{
     containerIn.classList.add('inactivo')
     containerE.classList.remove('inactivo')
+})
+
+btnRegistroE.addEventListener('click', ()=> {
+    let icono
+    switch (egresosTipo.value) {
+        case 'mercaderia':
+            icono= mercaderia;
+            break 
+        case 'medico':
+            icono= medico;
+            break 
+        case 'transporte':
+            icono= transporte;
+            break 
+        case 'lujo':
+            icono= lujo;
+            break 
+        case 'imprevisto':
+            icono= imprevisto;
+            break 
+        default:
+            icono=otros;
+            break 
+    }; 
+    let auxiliar = new Egreso(egresos.value, egresos.id, egresosNombre.value, icono, basura, id)
+    console.log(auxiliar);
+    auxiliar.insertar()
+    auxiliar.leerYEscribir()
+    auxiliar.elemento()
+    auxiliar.addevento(id)
+    egresos.value = ""
+    id+=1
 })
